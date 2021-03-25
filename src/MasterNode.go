2066bcde773816f724c1183ddfd1529003156dc5
Richie Zins <rizins@pop-os.localdomain>
2021-03-24 17:22:24 -0700

    Implemented tcp messaging over TLS

--- src/MasterNode.go
+++ src/MasterNode.go
@@ -1,27 +1,59 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"crypto/rand"
+	"crypto/tls"
 	"net"
+	"log"
 )
 
 func main() {
 	//6633 is node on the keypad
-	connThread, err := net.Listen("tcp", "localhost:6633")
-	 if err != nil {
-	 	fmt.Println("Error listening:", err.Error())
-	 	os.Exit(1)
-	 }
+	//connThread, err := net.Listen("tcp", "localhost:6633")
+	 //if err != nil {
+	 	//fmt.Println("Error listening:", err.Error())
+	 	//os.Exit(1)
+	 //}
 	 
-	 defer connThread.Close()
+	 //defer connThread.Close()
 
-	 for {
-	 	connection, err := connThread.Accept()
-	 	if err != nil {
-	 		fmt.Println("Error connecting:", err.Error())
-	 		return
-	 	}
-	 	fmt.Println("Client connected! Their addr is: " + connection.RemoteAddr().String())
-	 }
+	 //for {
+	 	//connection, err := connThread.Accept()
+	 	//if err != nil {
+	 		//fmt.Println("Error connecting:", err.Error())
+	 		//return
+	 	//}
+	 	//fmt.Println("Client connected! Their addr is: " + connection.RemoteAddr().String())
+	//}
+
+	cert, err := tls.LoadX509KeyPair("server.pem", "server.key")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := tls.Config{Certificates: []tls.Certificate{cert}, ClientAuth: tls.RequireAnyClientCert}
+	config.Rand = rand.Reader
+     
+	listen, err := tls.Listen("tcp", "localhost:6633", &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	log.Printf("Server(TLS) up and listening on port 6633")
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+		go handleConnection(conn)
+	}
 }
+
+func handleConnection(c net.Conn) {
+	log.Printf("Client connected! Their addr is: " + c.RemoteAddr().String())
+}
+
+
--- src/Node.go
+++ src/Node.go
@@ -1,18 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"net"
+	"crypto/tls"
+	"log"
 )
 
 func main() {
 	//6633 is node on the keypad
-	connThread, err := net.Dial("tcp", "localhost:6633")
-	 if err != nil {
-	 	fmt.Println("Error connecting:", err.Error())
-	 	os.Exit(1)
-	 }
+	//connThread, err := net.Dial("tcp", "localhost:6633")
+	 //if err != nil {
+	 	//fmt.Println("Error connecting:", err.Error())
+	 	//os.Exit(1)
+	 //}
 
-	 defer connThread.Close()
+	 //defer connThread.Close()
+    cert, err := tls.LoadX509KeyPair("client.pem", "client.key")
+    if err != nil {
+    	log.Fatal(err)
+    }
+
+	ip := "localhost"
+	port := "6633"
+
+	log.Printf("Connecting to %s\n", ip)
+
+	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
+	conn, err := tls.Dial("tcp", ip+":"+port, &config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+	log.Printf("Connection established between %s and localhost.\n", conn.RemoteAddr().String())    
 }
